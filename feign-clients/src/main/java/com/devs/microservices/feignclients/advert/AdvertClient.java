20c5c392ffc1eed3a77c9199a2436130893e22de
Azad Can Tekin <tekin.act@gmail.com>
2023-02-27 15:08:53 +0300

    ApiGateway config  , fix url , load balancing .

--- feign-clients/src/main/java/com/devs/microservices/feignclients/advert/AdvertClient.java
+++ feign-clients/src/main/java/com/devs/microservices/feignclients/advert/AdvertClient.java
@@ -3,7 +3,7 @@ package com.devs.microservices.feignclients.advert;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "ADVERT-SERVICE" )
+@FeignClient(name = "advert-service" )
 public interface AdvertClient {
 
     @PostMapping(path = "api/v1/advert/add-advert")
